e9cc77804df4eb04ae618782d06078aa08663c51
Ife Abraham <ife04abraham@gmail.com>
2026-09-08 09:04:28 +0100

    Updated database Url

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,20 @@ import { defineConfig } from "@playwright/test";
 // });
 
 
+// export default defineConfig({
+//   testDir: "./e2e",
+//   fullyParallel: false,
+//   retries: 0,
+//   reporter: "list",
+//   use: {
+//     baseURL:
+//       process.env.E2E_API_BASE_URL ||
+//       process.env.NEXT_PUBLIC_API_BASE_URL ||
+//       "https://willowy-zorana-swifttech-4219e978.koyeb.app/api/v1",
+//   },
+// });
+
+
 export default defineConfig({
   testDir: "./e2e",
   fullyParallel: false,
@@ -23,6 +37,6 @@ export default defineConfig({
     baseURL:
       process.env.E2E_API_BASE_URL ||
       process.env.NEXT_PUBLIC_API_BASE_URL ||
-      "https://willowy-zorana-swifttech-4219e978.koyeb.app/api/v1",
+      "https://koboplan-backend.up.railway.app",
   },
 });
--- src/lib/product.ts
+++ src/lib/product.ts
@@ -5,6 +5,10 @@ export const PRODUCT_NAME =
 //   process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ||
 //   "http://localhost:4000/api/v1";
 
+// export const API_BASE_URL =
+//   process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ||
+//   "https://willowy-zorana-swifttech-4219e978.koyeb.app/api/v1";
+
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ||
-  "https://willowy-zorana-swifttech-4219e978.koyeb.app/api/v1";
+  "https://koboplan-backend.up.railway.app";
